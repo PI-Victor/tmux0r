6126dd7300d1e157826970bfeeafdce1b58e8531
pi-victor <ipalade@redhat.com>
2015-10-21 23:12:01 +0200

    basic app with click

---
+++ tmx.py
@@ -0,0 +1,18 @@
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option('--dirconfig')
+def create(dirconfig):
+    pass
+
+@cli.command()
+def clean():
+    pass
+
+if __name__ == '__main__':
+    cli()

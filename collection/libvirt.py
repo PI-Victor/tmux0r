05c6eeb99684fddddc9ea2c293d987d622b0f09c
pi-victor <ipalade@redhat.com>
2015-11-06 15:38:06 +0100

    changed doc string

--- collection/libvirt.py
+++ collection/libvirt.py
@@ -2,8 +2,9 @@ import libvirt
 
 
 class LibVirtConn(object):
-    '''Provides an interface to a libvirt connection
-    '''
+    """Provides an interface to a libvirt
+    connection
+    """
     def __init__(self,libvirtdriver=None):
         try:
             libvirtdriver = 'qemu:///system'
@@ -14,34 +15,38 @@ class LibVirtConn(object):
         self.hypervisor = self.libvirthandler.getHostname()
 
     def list_domains(self):
-        '''Returns a list of defined domains on the host
-        that you can manipulate.
-        '''
+        """Returns a list of defined domains
+        on the host that you can manipulate.
+        """
         libvirt_domains = [libvirt for libvirt in self.libvirthandler.listDomainsID() if self.libvirthandler]
         return libvirt_domains
 
     def setup_domain(self, domainid):
-        '''Setup a new domaing with the specified settings'''
+        """Setup a new domaing with the
+        specified settings
+        """
         pass
 
     def destroy_domain(self, domainid):
-        '''Destroy a given domain for rebuilding'''
+        """Destroy a given domain"""
         pass
 
     def cpu_stats(self):
-        '''Get cpu stats'''
+        """Get cpu stats"""
         pass
 
     def network_stats(self):
-        '''Get network stats'''
+        """Get network stats"""
         pass
 
     def domain_stats(self, domainid=None):
-        '''Get all domain stats'''
+        """Get all domain stats"""
         pass
 
     def load_xml(self):
-        '''Load the predefined xml file into the object'''
+        """Loads the predefined xml file into the
+        object for manipulation
+        """
         pass
 
     def get_domain_state(self):
@@ -51,43 +56,49 @@ class LibVirtConn(object):
         return self.libvirthandler.listAllDomains()
 
     def close_connection(self):
-        '''Close current hypervisor connection'''
+        """Close current hypervisor connection"""
         self.libvirthandler.close()
 
     def create_snapshot(self, domainid):
-        '''Create a snapshot of the given domain'''
+        """Create a snapshot of the given domain"""
         pass
 
     def restore_snapshot(self, domainid, snapshotid):
-        '''Restore a specific domain'''
+        """Restore a specific domain"""
         pass
 
     def get_snapshots(self, domainid):
-        '''Get current snapshots of given domain'''
+        """Get current snapshots of given domain"""
         pass
 
     def restart_domain(self, domainid):
-        '''Restart a given domain'''
+        """Restart a given domain"""
         pass
 
     def stop_domain(self, domainid):
-        '''Stop a given domain'''
+        """Stop a given domain"""
         pass
 
     def start_domain(self, domainid):
-        '''Start a given domain'''
+        """Start a given domain"""
         pass
 
     def suspend_domain(self, domainid):
-        '''Suspend a given domain'''
+        """Suspend a given domain"""
         pass
 
     def save_domain(self, domainid):
-        '''Save the state of a domain and shut it down'''
+        """Save the state of a domain and
+        shut it down
+        """
         pass
 
     def reset_domain(self, domainid):
+        """Force reset a domain"""
         pass
 
     def resume_domain(self, domaind):
+        """Resume a domain that has been
+        paused
+        """
         pass

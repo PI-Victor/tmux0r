d230028b8ab49f5c3dac58a1a6cc844f287c3c0f
PI-Victor <ipalade@redhat.com>
2015-11-06 22:52:15 +0100

    basic openshift wrapper class

---
+++ collection/openshift.py
@@ -0,0 +1,16 @@
+
+
+class OpenShiftWrapper(object):
+    """A wrapper around openshift's commands
+    """
+    def __init__(self):
+        pass
+
+    def create_pod(self):
+        pass
+
+    def create_project(self):
+        pass
+
+    def run_tests(self):
+        pass

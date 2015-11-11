e9158185b15d98be51731fa3e0280cca32bc9b47
pi-victor <ipalade@redhat.com>
2015-11-11 11:59:43 +0100

    docstrings and other wrapper functions for tmux

--- collection/tmux.py
+++ collection/tmux.py
@@ -1,28 +1,55 @@
 import tmuxp
 
 
+BASH_BIN_DEFAULT="/usr/bin/bash"
+
+
 class TmuxWrapper(object):
+    """A wrapper around the tmuxp lib to
+    manipulate tmux from the cli with the help
+    of the pocoo click lib.
+    """
 
     def __init__(self, findid, createservice):
         pass
 
     def window_factory(self, sessionid):
-        """create a new window split in a
-        given window id
+        """Create a new window split in a
+        given window id.
         """
         pass
 
-    def close_window_split(self, sessionid):
+    def close_window_split(self, windowid=None):
+        """Close a particular split in the current
+        window or close all of them if windowid is
+        not passed
+        """
         pass
 
-    def tab_factory(self, sessionid):
-        """create a new pane"""
+    def pane_factory(self, windowid=None):
+        """Create a new pane with given content
+        if nothing is passed, just open a pane with bash
+        """
+        pass
+
+    def close_pane(self, paneid=None):
+        """Close given pane in the session,
+        if no pane is given, close all panes
+        in the active session
+        """
         pass
 
     def get_active_sessions(self):
-        """return the number of active sessions"""
+        """Return the number of active sessions"""
         pass
 
     def attach_to_session(self, sessionid):
-        """attach to a specific session"""
+        """Attach to a specific session"""
+        pass
+
+    def filter_output(self, sessionid):
+        """Filter the output of a given window/pane
+        This comes in handy for on-the-fly monitoring
+        of tests in or outside of container/vms
+        """
         pass

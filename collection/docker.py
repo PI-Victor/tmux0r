20ff6849670d46f43c7d783167defff44de9c168
pi-victor <ipalade@redhat.com>
2015-11-11 11:58:12 +0100

    added docstrings and a few useful functions to the docker wrapper

--- collection/docker.py
+++ collection/docker.py
@@ -1,42 +1,94 @@
 import docker
 
 
+DEFAULT_DOCKER_SOCKET='unix://var/run/docker.sock'
+
+
 class DockerWrapper(object):
-    def __init__(self, docker_socket='unix://var/run/docker.sock'):
+    def __init__(self, docker_socket=DEFAULT_DOCKER_SOCKET):
         self.dockerhandler = docker.Client(base_url=docker_socket)
 
-    def provision_container(self, image_id):
+    def provision_container(self, image_id, **kwargs):
+        """Create a new container based on an image_id
+        """
         pass
 
-    def list_containers(self, image_id=None):
+    def list_containers(self, image_id=None, *args):
+        """List all containers containers.
+        :image_id -  if it's passed, it shows containers
+        that are bound to that docker image.
+        :*args - stopped: list only stopped containers
+        - running: list only running containers.
+        """
         return self.dockerhandler.containers()
 
-    def stop_container(self):
+    def stop_container(self, containerid=None):
+        """Stop a given container, otherwise stop
+        all running containers.
+        """
         pass
 
     def remove_container(self, container_id=None):
+        """Remove a given container, otherwise remove
+        all containers, running or not.
+        """
         pass
 
     def attach_to_container(self, container_id):
+        """Attach to a bash session of a given running
+        container
+        """
         pass
 
     def run_container_command(self, container_id):
+        """Run a command inside a given container
+        and output the result.
+        """
         pass
 
     def get_container_log(self, container_id):
+        """Return the full log of the specified
+        container.
+        """
         pass
 
-    def refresh_image(self, image_name=None):
+    def refresh_image(self, image_name=None, registry_name=None):
+        """Pull the image with the :latest tag,
+        if no image is given, refresh all images.
+        :registry_name - from a given registry
+        default docker registry if not passed.
+        """
         pass
 
-    def pull_image(self, image_name, image_tag='latest'):
+    def get_image(self, image_name, image_tag='latest', registry_name=None):
+        """Pull a given image with a given tag,
+        default is :latest.
+        :registry_name - default docker registry
+        if nothing is passed.
+        """
         pass
 
-    def repack_image(self, image_name):
+    def repack_image(self, image_name, copyto=None):
+        """Repack a given image, and copy a file, or a
+        list of files inside it
+        """
         pass
 
-    def remove_image(self, image_id=None):
+    def remove_images(self, image_id=None):
+        """If no image was specified, either remove a set
+        of images or all images
+        """
         pass
 
-    def list_images(self):
+    def list_images(self, notag=False):
+        """List all docker images
+        :notag - return a list of images with a <none>
+        tag so that they can be removed
+        """
+        pass
+
+    def tag_image(self, image_id, tag_name=None):
+        """Tag an image
+        """
+        default_tag = "latest"
         pass

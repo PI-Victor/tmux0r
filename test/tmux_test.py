7e49d0b04db972a74f8bc096010fe71116be73eb
Victor <ipalade@redhat.com>
2017-01-24 10:53:51 +0100

    add tmuxor test

--- test/tmux_test.py
+++ test/tmux_test.py
@@ -13,6 +13,7 @@ def test_session_factory():
     tmwrap.session_factory()
     sessions = tmwrap.get_sessions()
     assert sessions is not None
+    for s in sessions:
     tmwrap.close_sessions()
 
 def test_window_factory():
@@ -22,11 +23,11 @@ def test_window_factory():
     for session in tmwrap.get_sessions():
         tmwrap.window_factory(session)
 
+# TODO: remake this part of the test, it's invalid.
+# get_windows returns an empty list.
     for s in tmwrap.get_sessions():
         windows = tmwrap.get_windows(s)
         for w in windows:
             assert isinstance(w, tmuxp.window.Window)
 
     tmwrap.close_sessions()
-
-#tmwrap.close_sessions()
